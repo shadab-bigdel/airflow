319439763ab4b0bd4372d0a7a57622489385b662
SWORDGROUP\sbigdel <shadab.bigdel@sword-group.com>
2024-09-08 08:15:44 +0200

    no message

--- dags/hello_world_Log_dag.py
+++ dags/hello_world_Log_dag.py
@@ -1,22 +1,34 @@
-import logging
 from airflow import DAG # type: ignore
-from airflow.operators.python_operator import PythonOperator # type: ignore
+from airflow.operators.python import PythonOperator # type: ignore
 from datetime import datetime
+import logging
 
-# Set up logging
-logger = logging.getLogger("airflow.task")
+# Define functions for tasks
+def log_hello_message():
+    logging.info("Hello log from DAG")
 
-def my_custom_function():
-    logger.info("This is a custom log message from my DAG task.")
-    print("This will also appear in the task logs.")
+def log_goodbye_message():
+    logging.info("Goodbye log from DAG")
 
-default_args = {
-    'owner': 'airflow',
-    'start_date': datetime(2023, 1, 1),
-}
-
-with DAG('hello_world_Log_dag', default_args=default_args, schedule_interval='@daily') as dag:
-    task = PythonOperator(
-        task_id='hello_world_Log',
-        python_callable=my_custom_function,
+# Define the DAG
+with DAG(
+    dag_id='manual_hello_log_dag',
+    start_date=datetime(2023, 9, 8),
+    schedule_interval=None,
+    catchup=False,
+    tags=['example'],
+) as dag:
+    
+    # Define tasks
+    log_hello_task = PythonOperator(
+        task_id='log_hello_message',
+        python_callable=log_hello_message,
     )
+
+    log_goodbye_task = PythonOperator(
+        task_id='log_goodbye_message',
+        python_callable=log_goodbye_message,
+    )
+
+    # Set task dependencies
+    log_hello_task >> log_goodbye_task
