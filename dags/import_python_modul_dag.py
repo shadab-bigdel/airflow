c97dd8579bfe79c20aecfda39ff7f855f6e69c8d
SWORDGROUP\sbigdel <shadab.bigdel@sword-group.com>
2024-09-30 13:39:04 +0200

    add dags for testing import python modules

---
+++ dags/import_python_modul_dag.py
@@ -0,0 +1,44 @@
+#import os
+#import sys
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+import logging
+
+# Add the parent directory to the Python path to import the module
+#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# Import the external Python module
+from my_module import process_data
+
+# Function to call the external module and log the output
+def task_with_logging():
+    logging.info("Starting the task")
+    
+    # Call the function from the external module
+    result = process_data()
+    
+    # Log the result
+    logging.info(f"Result from process_data: {result}")
+    logging.info("Task finished")
+
+# Define the default_args for the DAG
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 9, 1),
+    'retries': 1,
+}
+
+# Create the DAG
+with DAG(dag_id='import_python_module_dag',  # Updated DAG name
+         default_args=default_args,
+         schedule_interval='@daily',
+         catchup=False) as dag:
+
+    # Define the PythonOperator
+    task1 = PythonOperator(
+        task_id='log_info_task',
+        python_callable=task_with_logging
+    )
+
+    task1
---
+++ my_modul.py
@@ -0,0 +1,10 @@
+from airflow.models import Variable
+import logging
+
+
+# my_module.py
+
+def process_data():
+    # Some logic
+    return "Data processed successfully"
+
